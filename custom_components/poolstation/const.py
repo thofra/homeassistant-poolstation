23b0af9cd753b03a98194bf07944fa6a3242ced6
Straybiker <stray@telenet.be>
2026-04-20 19:56:24 +0200

    2FA support

--- custom_components/poolstation/const.py
+++ custom_components/poolstation/const.py
@@ -3,6 +3,7 @@ from typing import Final
 
 DOMAIN: Final = "poolstation"
 TOKEN: Final = "token"
+CONF_AUTH_CODE: Final = "auth_code"
 COORDINATORS: Final = "coordinators"
 DEVICES: Final = "devices"
 AUTH_RETRIES:  Final[int] = 10
